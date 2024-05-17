573e5f708e8a108151897a5f8deda7b3d6e9a91d
Bimbalas112 <faustas.sindaras1@gmail.com>
2024-05-17 16:04:40 +0100

    updated

--- testes.py
+++ testes.py
@@ -16,25 +16,25 @@ from BluEase import BluEase
 import DeterminacaoDaSala
 import time
 
-# Observações:
-# - Os botões fora da parte visível não funcionam, por isso é que tentei criar animações:
-#     - Tentou-se começar a aplicação com todos os botões no ecrã e depois fazer zoom, mas não resultou :)
-#     - As configurações dos botões para a animação são: size=(100/2,100/2),pos=(size_x/2, size_y/2)
-#     - As configrações para a imagem da planta são: size=(600, 600)
+'''Observações:
+- Os botões fora da parte visível não funcionam, por isso é que tentei criar animações:
+    - Tentou-se começar a aplicação com todos os botões no ecrã e depois fazer zoom, mas não resultou :)
+    - As configurações dos botões para a animação são: size=(100/2,100/2),pos=(size_x/2, size_y/2)
+    - As configrações para a imagem da planta são: size=(600, 600)'''
 
 Window.size = (312, 665)
 
-# Carrega as funcionalidades do backend
+# carrega as funcionalidades do backend
 bluease = BluEase()
 
-# Guarda o edifício atual na base de dados (ISCTE-Sintra) em uma variável
+# guarda o edifício atual na base de dados (ISCTE-Sintra) em uma variável
 building = bluease.get_building()
 
 class Bluey(App):
     def build(self):
         root = RelativeLayout()
-        Window.clearcolor = (1, 1, 1, 1)
-        inicio = Image(source='bluething.png', allow_stretch=True, keep_ratio=True, pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        Window.clearcolor = (1,1,1,1)
+        inicio = Image(source='bluething.png', allow_stretch=True, keep_ratio=True,pos_hint={'center_x': 0.5, 'center_y': 0.5})
         root.add_widget(inicio)
         return root
 
@@ -48,33 +48,31 @@ class Andar1(App):
         self.scatter.add_widget(map_image)
         self.button_layout = RelativeLayout(size=(map_image.size))
 
-        self.spot_buttons = {}  # Dictionary to keep track of spot buttons
-
         # Minha mudança foi aqui, ao invés de ir naqueles arrays pegar as informações, pedes elas ao backend
         # Para cada spot (Receção, corredor lateral, entrada, etc) dentro do edificio cria o botão para ele
         for spot in building.get_spots():
             icon = "rsz_1rsz_interestpoint.png"
-            size = (70, 70)
-            if spot.get_name() == DeterminacaoDaSala.my_variable:
+            size = (70,70)
+            if spot.get_name() ==  DeterminacaoDaSala.my_variable:
                 icon = "rsz_1interestpoint_red.png"
-                size = (55, 68)
-            spot_button = Button(
+                size = (55,68)
+            self.spot_button = Button(
                 size_hint=(None, None),
-                size=size,
+                size=size,                          
                 pos=spot.get_pos(),
                 background_normal=icon,
                 background_down=icon,
                 on_press=lambda instance, nome=spot.get_name(), pop=spot.get_pictures(): self.on_press(instance, nome, pop))
-            self.spot_buttons[spot.get_name()] = spot_button  # Store the button in the dictionary
-            self.button_layout.add_widget(spot_button)
+            # TODO: Falta criar algo que receba a sala atual e altere a cor do botão de azul para vermelha
+            self.button_layout.add_widget(self.spot_button)
 
             # Para cada ponto de interesse (Máquina de vendas, balcão de informação, etc) dentro do spot, cria o botão para ele
             for interestpoint in spot.getInterest_points():
-                if interestpoint.getType() in ["Escadas", "Elevador"]:
-                    size_ = (65, 65)
+                if interestpoint.getType() in ["Escadas","Elevador"]:
+                    size_ = (65,65)
                 else:
-                    size_ = (85, 85)
-                ip_button = Button(
+                    size_ = (85,85)
+                self.ip_button = Button(
                     size_hint=(None, None),
                     size=size_,
                     pos=interestpoint.get_pos(),
@@ -82,8 +80,17 @@ class Andar1(App):
                     background_down=interestpoint.get_icon(),
                     on_press=lambda instance, nome=interestpoint.get_description(), pop="": self.on_press(instance, nome, pop)
                 )
-                self.button_layout.add_widget(ip_button)
-
+                self.button_layout.add_widget(self.ip_button)
+        """
+        # NODES NOVO
+        for node in bluease.get_nodes():
+            ponto=Button(
+                size=(30,30),
+                pos=(node[1],node[2]),
+                background="rsz_1interestpoint_red.png"
+            )
+            self.button_layout.add_widget(ponto)
+        """
         self.scatter.add_widget(self.button_layout)
 
         logo_overlay = BoxLayout(orientation='horizontal', size_hint=(None, None), size=(Window.width, 130), pos=(0, Window.height))
@@ -94,17 +101,17 @@ class Andar1(App):
         logo = Image(source='BluEase_logo_sem_fundo.png', size_hint=(None, None), size=(120, 120))
         logo_overlay.add_widget(logo)
 
-        menu = Image(source='icons/icons8-menu-100.png', size_hint=(None, None), size=(70, 70), pos_hint={'center_x': 0.9, 'center_y': 0.95})
-        reg = Label(text='1º Andar', font_size=50, color=(1, 1, 1, 1), size_hint=(0.1, 0.05), pos_hint={'center_x': 0.5})
+        menu = Image(source='icons/icons8-menu-100.png', size_hint=(None, None), size=(70,70), pos_hint={'center_x': 0.9, 'center_y': 0.95})
+        reg = Label(text='1º Andar', font_size=50, color=(1,1,1, 1), size_hint=(0.1, 0.05), pos_hint={'center_x': 0.5})
 
         search = Button(background_normal='icons/icons8-search-100-2.png',
                         background_down='icons/icons8-search-100-3.png',
                         on_press=self.show_search_popup, size_hint=(None, None),
-                        size=(100, 100), pos_hint={'center_x': 0.9, 'center_y': 0.16})
+                size=(100,100), pos_hint={'center_x': 0.9, 'center_y': 0.16})
         flash = Button(background_normal='icons/icons8-flash-100.png',
                        background_down='icons/icons8-flash-100-2.png',
                        on_press=self.show_quick_search_popup, size_hint=(None, None),
-                       size=(100, 100), pos_hint={'center_x': 0.9, 'center_y': 0.07})
+                size=(100,100), pos_hint={'center_x': 0.9, 'center_y': 0.07})
 
         root.add_widget(self.scatter)
         root.add_widget(logo_overlay)
@@ -120,25 +127,27 @@ class Andar1(App):
         reg_layout.add_widget(space_2_reg_layout)
         root.add_widget(reg_layout)
 
-        # Animação
+# Animação
         '''anim = Animation(width=map_image.width * 2, height=map_image.height * 2, transition='out_circ', duration=2)
         anim.start(map_image)'''
 
         return root
 
+
     def show_path(self):
         with self.root.canvas:
-            coordinates = bluease.wayfind("5", "26")
+            coordinates = bluease.wayfind("5","26")
             print(coordinates)
             with self.scatter.canvas:
                 Color(1, 0, 0)
                 for i in range(len(coordinates) - 1):
                     # Draw lines connecting coordinates
                     Line(points=[int(bluease.get_nodes()[coordinates[i]][0]), int(bluease.get_nodes()[coordinates[i]][1]),
-                                 int(bluease.get_nodes()[coordinates[i + 1]][0]), int(bluease.get_nodes()[coordinates[i + 1]][1])])
+                                 int(bluease.get_nodes()[coordinates[i+1]][0]), int(bluease.get_nodes()[coordinates[i+1]][1])])
+
 
     def on_directions_button_press(self, instance, nome, pop):
-        # Fecha o popup e chama a função faustas
+        # fecha o popup e chama a função faustas
         pop.dismiss()
         self.show_path()
 
@@ -147,31 +156,30 @@ class Andar1(App):
         image = Image(source=pop, size_hint=(1, 0.8), allow_stretch=True)
 
         button1 = Button(text='Informações', size_hint_y=None, height=50)
-        # Chama a função acima
+# chama a função acima
         button2 = Button(text='Direções', size_hint_y=None, height=50)
 
         content.add_widget(image)
         content.add_widget(button1)
         content.add_widget(button2)
         popup = Popup(title=f'{nome}', content=content, size_hint=(0.8, 0.6), separator_color=[0, 57 / 255, 1, 1],
-                      background_color=[1, 1, 1, 1])
+                          background_color=[1, 1, 1, 1])
         button2.bind(on_press=lambda instance: self.on_directions_button_press(instance, nome, popup))
 
         popup.open()
 
+        # background='bluething.png'
     def show_search_popup(self, instance):
-        search_popup = SearchPopup(title='Procurar pontos de interesse', size_hint=(0.6, 0.4))
-        search_popup.open()
+            search_popup = SearchPopup(title='Procurar pontos de interesse', size_hint=(0.6, 0.4))
+            search_popup.open()
 
     def show_quick_search_popup(self, instance):
-        quick_search_popup = QuickSearchPopup(title='Procura rápida', size_hint=(0.6, 0.6))
-        quick_search_popup.open()
+            quick_search_popup = QuickSearchPopup(title='Procura rápida', size_hint=(0.6, 0.6))
+            quick_search_popup.open()
+# background='bluething.png'
 
-    def change_button_color(self, spot_name, new_icon):
-        if spot_name in self.spot_buttons:
-            button = self.spot_buttons[spot_name]
-            button.background_normal = new_icon
-            button.background_down = new_icon
+    
+
 
 class SearchPopup(Popup):
     def __init__(self, **kwargs):
@@ -196,6 +204,5 @@ class QuickSearchPopup(Popup):
 if __name__ == '__main__':
     DeterminacaoDaSala.ButtonUIApp().run()
     Andar1().run()
-
-    # Change the color of a button dynamically
-Andar1().change_button_color("Receção", "rsz_1interestpoint_red.png")
+    
+    
